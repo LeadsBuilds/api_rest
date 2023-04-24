820a6037c8b17eea8ab21ac973e28204cf1219c4
LeadsBuilds <dell50cent@gmail.com>
2023-04-24 20:48:33 -0300

    Add .env

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,11 @@
+import * as dotenv from 'dotenv';
 import mongoose from "mongoose";
 
-mongoose.connect("connection string")
+dotenv.config();
+
+const connectionString = process.env.MONGODB;
+
+mongoose.connect(connectionString);
 const db = mongoose.connection;
 
 export default db;
